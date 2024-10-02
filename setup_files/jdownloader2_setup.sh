fd0d25d227a56a9436e2ec4367ebec32177632c9
PronPan <gerbildude124@gmail.com>
2024-10-02 01:39:08 -0700

    Update jdownloader2_setup.sh

--- setup_files/jdownloader2_setup.sh
+++ setup_files/jdownloader2_setup.sh
@@ -2,80 +2,121 @@
 
 export LANG=en_US.UTF-8
 
+# Enable detailed tracing for debugging
 set -o errexit 
 set -o errtrace
 set -o nounset 
-set -o pipefail 
+set -o pipefail
+set -x  # Enable command tracing (prints commands before executing)
+
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-CHECKMARK='\033[0;32m\xE2\x9C\x94\033[0m'
 trap 'die "Script interrupted."' INT
 
+# Logging setup
+DEBUG_LOG="/var/log/jdownloader2_setup_debug.log"
+exec > >(tee -a "$DEBUG_LOG") 2>&1  # Redirect all output and errors to the debug log
+
+CHECKMARK='\033[0;32m\xE2\x9C\x94\033[0m'
+
+# Function to output error details
 function error_exit() {
-  trap - ERR
-  local DEFAULT='Unknown failure occurred.'
-  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
-  local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
-  msg "$FLAG $REASON"
-  exit $EXIT
+    trap - ERR
+    local DEFAULT='Unknown failure occurred.'
+    local REASON="\e[97m${1:-$DEFAULT}\e[39m"
+    local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
+    msg "$FLAG $REASON"
+    echo "Exiting with error code $EXIT at line $LINE" >> "$DEBUG_LOG"
+    exit $EXIT
 }
+
+# Log general message
 function msg() {
-  local TEXT="$1"
-  echo -e "$TEXT"
+    local TEXT="$1"
+    echo -e "$TEXT"
+    echo "$(date): $TEXT" >> "$DEBUG_LOG"
 }
 
-echo -e "${CHECKMARK} \e[1;92m Setting up container OS... \e[0m"
-sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
-locale-gen >/dev/null
-apt-get purge openssh-{client,server} >/dev/null
-apt-get autoremove >/dev/null
+# Log informational messages
+function info() {
+    local REASON="$1"
+    local FLAG="\e[36m[INFO]\e[39m"
+    msg "$FLAG $REASON"
+}
 
-echo -e "${CHECKMARK} \e[1;92m Updating container OS... \e[0m"
-apt-get update &>/dev/null
-apt-get -qq upgrade &>/dev/null
+# Start of the main process
+info "Starting jDownloader2 setup script."
 
-echo -e "${CHECKMARK} \e[1;92m Installing prerequisites... \e[0m"
-apt-get -qq install \
-    wget \
-    sudo \
-    openjdk-17-jre-headless &>/dev/null
+# Locale configuration
+info "Uncommenting locale in /etc/locale.gen for $LANG."
+sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen || die "Failed to uncomment locale."
+info "Generating locale."
+locale-gen >/dev/null || die "Failed to generate locale."
 
-echo -e "${CHECKMARK} \e[1;92m Creating user for jdownloader2... \e[0m"
-useradd -s /sbin/nologin jdown2
+# Purge OpenSSH packages
+info "Purging OpenSSH client and server."
+apt-get purge openssh-{client,server} >/dev/null || die "Failed to purge OpenSSH client/server."
 
-echo -e "${CHECKMARK} \e[1;92m Creating folder for jdownloader2... \e[0m"
-mkdir /opt/jdown2
-chown jdown2 /opt/jdown2
-cd /opt/jdown2
+# Autoremove unnecessary packages
+info "Running apt-get autoremove to clean up unnecessary packages."
+apt-get autoremove >/dev/null || die "Failed to autoremove unnecessary packages."
 
-echo -e "${CHECKMARK} \e[1;92m Downloading jdownloader2... \e[0m"
-sudo -u jdown2 wget http://installer.jdownloader.org/JDownloader.jar
-wget -O /etc/systemd/system/jdownloader2.service https://raw.githubusercontent.com/pronpan/proxmox-scripts/main/systemd_files/jdownloader2.service &>/dev/null
+# Updating the container OS
+info "Updating package lists."
+apt-get update || die "Failed to update package lists."
 
-echo -e "${CHECKMARK} \e[1;92m Enabling systemd service for jdownloader2... \e[0m"
-systemctl daemon-reload &>/dev/null
-systemctl enable jdownloader2 &>/dev/null
+info "Upgrading packages to the latest versions."
+apt-get upgrade || die "Failed to upgrade packages."
 
-#echo -e "${CHECKMARK} \e[1;92m Setting up NFS share for the jdownloader2 Downloads folder... \e[0m"
-#wget -O /etc/exports https://raw.githubusercontent.com/pronpan/proxmox-scripts/main/config_files/exports &>/dev/null
-#systemctl restart nfs-kernel-server &>/dev/null
+# Installing prerequisites
+info "Installing prerequisites: wget, sudo, and openjdk-17-jre-headless."
+apt-get install wget sudo openjdk-17-jre-headless || die "Failed to install prerequisites."
 
-#echo -e "${CHECKMARK} \e[1;92m Disabling NFS server... \e[0m"
-#systemctl disable --now nfs-kernel-server &>/dev/null
+# Create user for jdownloader2
+info "Creating user 'jdown2' with no login shell."
+useradd -s /sbin/nologin jdown2 || die "Failed to create user 'jdown2'."
 
-echo -e "${CHECKMARK} \e[1;92m Customizing container... \e[0m"
-rm /etc/motd 
-rm /etc/update-motd.d/10-uname 
-touch ~/.hushlogin 
+# Create folder for jdownloader2
+info "Creating directory /opt/jdown2 for jdownloader2."
+mkdir /opt/jdown2 || die "Failed to create directory /opt/jdown2."
+info "Changing ownership of /opt/jdown2 to jdown2."
+chown jdown2 /opt/jdown2 || die "Failed to change ownership of /opt/jdown2."
+
+# Downloading jdownloader2
+info "Downloading JDownloader.jar for jdownloader2."
+sudo -u jdown2 wget http://installer.jdownloader.org/JDownloader.jar || die "Failed to download JDownloader.jar."
+
+# Download systemd service file for jdownloader2
+info "Downloading systemd service file for jdownloader2."
+wget -O /etc/systemd/system/jdownloader2.service https://raw.githubusercontent.com/pronpan/proxmox-scripts/main/systemd_files/jdownloader2.service >/dev/null || die "Failed to download systemd service file."
+
+# Enable systemd service for jdownloader2
+info "Reloading systemd daemon."
+systemctl daemon-reload || die "Failed to reload systemd daemon."
+info "Enabling jdownloader2 systemd service."
+systemctl enable jdownloader2 || die "Failed to enable jdownloader2 service."
+
+# Customizing container
+info "Customizing container: Removing MOTD and other files."
+rm /etc/motd || die "Failed to remove /etc/motd."
+rm /etc/update-motd.d/10-uname || die "Failed to remove /etc/update-motd.d/10-uname."
+touch ~/.hushlogin || die "Failed to create ~/.hushlogin."
+
+# Configure autologin
+info "Configuring container autologin for root."
 GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
-mkdir -p $(dirname $GETTY_OVERRIDE)
+mkdir -p $(dirname $GETTY_OVERRIDE) || die "Failed to create directory for getty override."
 cat << EOF > $GETTY_OVERRIDE
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
 EOF
-systemctl daemon-reload
-systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
-echo -e "${CHECKMARK} \e[1;92m Cleanup... \e[0m"
-rm -rf /jdownloader2_setup.sh /var/{cache,log}/* /var/lib/apt/lists/*
+systemctl daemon-reload || die "Failed to reload systemd daemon after getty override."
+systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//') || die "Failed to restart getty service."
+
+# Cleanup
+info "Performing cleanup tasks."
+rm -rf /jdownloader2_setup.sh /var/{cache,log}/* /var/lib/apt/lists/* || die "Failed to clean up temporary files."
+
+info "jDownloader2 setup completed successfully."
