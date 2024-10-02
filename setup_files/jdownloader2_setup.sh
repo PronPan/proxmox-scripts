aae6556af940598492c9ec9ead1cbfe33c3693d9
PronPan <gerbildude124@gmail.com>
2024-10-02 01:04:56 -0700

    Update jdownloader2_setup.sh

--- setup_files/jdownloader2_setup.sh
+++ setup_files/jdownloader2_setup.sh
@@ -12,7 +12,7 @@ trap 'die "Script interrupted."' INT
 
 function error_exit() {
   trap - ERR
-  local DEFAULT='Unknown failure occured.'
+  local DEFAULT='Unknown failure occurred.'
   local REASON="\e[97m${1:-$DEFAULT}\e[39m"
   local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
   msg "$FLAG $REASON"
@@ -23,46 +23,46 @@ function msg() {
   echo -e "$TEXT"
 }
 
-echo -e "${CHECKMARK} \e[1;92m Setting up Container OS... \e[0m"
+echo -e "${CHECKMARK} \e[1;92m Setting up container OS... \e[0m"
 sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
 locale-gen >/dev/null
-apt-get -y purge openssh-{client,server} >/dev/null
+apt-get purge openssh-{client,server} >/dev/null
 apt-get autoremove >/dev/null
 
-echo -e "${CHECKMARK} \e[1;92m Updating Container OS... \e[0m"
+echo -e "${CHECKMARK} \e[1;92m Updating container OS... \e[0m"
 apt-get update &>/dev/null
-apt-get -qqy upgrade &>/dev/null
+apt-get -qq upgrade &>/dev/null
 
-echo -e "${CHECKMARK} \e[1;92m Installing Prerequisites... \e[0m"
-apt-get -qqy install \
+echo -e "${CHECKMARK} \e[1;92m Installing prerequisites... \e[0m"
+apt-get -qq install \
     wget \
     sudo \
-    openjdk-11-jre-headless &>/dev/null
+    openjdk-17-jre-headless &>/dev/null
 
-echo -e "${CHECKMARK} \e[1;92m Create user for jdownloader2... \e[0m"
+echo -e "${CHECKMARK} \e[1;92m Creating user for jdownloader2... \e[0m"
 useradd -s /sbin/nologin jdown2
 
-echo -e "${CHECKMARK} \e[1;92m Create folder for jdownloader2... \e[0m"
+echo -e "${CHECKMARK} \e[1;92m Creating folder for jdownloader2... \e[0m"
 mkdir /opt/jdown2
 chown jdown2 /opt/jdown2
 cd /opt/jdown2
 
 echo -e "${CHECKMARK} \e[1;92m Downloading jdownloader2... \e[0m"
 sudo -u jdown2 wget http://installer.jdownloader.org/JDownloader.jar
-wget -O /etc/systemd/system/jdownloader2.service https://raw.githubusercontent.com/StevenSeifried/proxmox-scripts/main/systemd_files/jdownloader2.service &>/dev/null
+wget -O /etc/systemd/system/jdownloader2.service https://raw.githubusercontent.com/pronpan/proxmox-scripts/main/systemd_files/jdownloader2.service &>/dev/null
 
-echo -e "${CHECKMARK} \e[1;92m Enable systemd service for jdownloader2... \e[0m"
-systemctl daemon-reload  &>/dev/null
-systemctl enable jdownloader2  &>/dev/null
+echo -e "${CHECKMARK} \e[1;92m Enabling systemd service for jdownloader2... \e[0m"
+systemctl daemon-reload &>/dev/null
+systemctl enable jdownloader2 &>/dev/null
 
-#echo -e "${CHECKMARK} \e[1;92m Setup NFS share for the jdownloader2 Downloads folder... \e[0m"
-#wget -O /etc/exports https://raw.githubusercontent.com/StevenSeifried/proxmox-scripts/main/config_files/exports &>/dev/null
+#echo -e "${CHECKMARK} \e[1;92m Setting up NFS share for the jdownloader2 Downloads folder... \e[0m"
+#wget -O /etc/exports https://raw.githubusercontent.com/pronpan/proxmox-scripts/main/config_files/exports &>/dev/null
 #systemctl restart nfs-kernel-server &>/dev/null
 
-#echo -e "${CHECKMARK} \e[1;92m Disable NFS server... \e[0m"
+#echo -e "${CHECKMARK} \e[1;92m Disabling NFS server... \e[0m"
 #systemctl disable --now nfs-kernel-server &>/dev/null
 
-echo -e "${CHECKMARK} \e[1;92m Customizing Container... \e[0m"
+echo -e "${CHECKMARK} \e[1;92m Customizing container... \e[0m"
 rm /etc/motd 
 rm /etc/update-motd.d/10-uname 
 touch ~/.hushlogin 
